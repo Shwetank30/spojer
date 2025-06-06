e5cf2fbf37235ac2f70401581d0b0043462bcc2d
Shwetank Kumar <shwetank.kmr@gmail.com>
2025-06-06 19:19:52 +0200

    intial code for arith SPOJ problem

---
+++ src/bin/arith.rs
@@ -0,0 +1,8 @@
+// Write a calculator that makes the output result formatted in the same way as usually used with manual calculations
+// With addition and subtraction, the numbers are written below each other.
+// Multiplication is a little bit more complex: first of all, we make a partial result for every digit of one of the numbers, and then sum the results together.
+
+use std::io::{self, BufRead};
+fn main() {
+    let stdin = io::stdin();
+}
